382186b26650ac16ea309b5e48b332ff185d77f2
Jennifer Lee <tocontactjen@gmail.com>
2022-08-09 15:53:02 -0400

    Appointment Controller completed

---
+++ capstone/java/src/main/java/com/techelevator/controller/AppointmentController.java
@@ -0,0 +1,47 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.AppointmentDao;
+import com.techelevator.model.Appointment;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class AppointmentController {
+
+    private AppointmentDao appointmentDao;
+
+    public AppointmentController(AppointmentDao appointmentDao) {
+        this.appointmentDao = appointmentDao;
+    }
+
+    @GetMapping("/doctors/{id}/appointments")
+    public List<Appointment> getAppointmentsByDoctor(@PathVariable long id) {
+        return appointmentDao.getAppointmentsByDoctor(id);
+    }
+
+    @GetMapping("/patients/{id}/appointments")
+    public List<Appointment> getAppointmentsByUser(@PathVariable long id) {
+        return appointmentDao.getAppointmentsByPatient(id);
+    }
+
+    @PostMapping("patients/{id}/appointments")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Appointment createAppointment(@RequestBody Appointment newAppointment) {
+        return appointmentDao.createAppointment(newAppointment);
+    }
+
+    @PutMapping("patients/{id}/appointments/{appointmentId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Appointment updateAppointment(@PathVariable("id") long id, @PathVariable("appointmentId") long appointmentId, @RequestBody Appointment updatedAppointment) {
+        updatedAppointment.setId(appointmentId);
+        if (appointmentDao.updateAppointment(updatedAppointment) != null){
+            return updatedAppointment;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Incorrect appointment ID. Please try again.");
+        }
+    }
+}
