189b5381b4ef4b74f76a8a27f8428f071c00c056
Cathy Ngo <cngo26@gmail.com>
2022-08-10 12:54:41 -0400

    reviEndpt

--- capstone/java/src/main/java/com/techelevator/controller/ReviewsController.java
+++ capstone/java/src/main/java/com/techelevator/controller/ReviewsController.java
@@ -4,6 +4,7 @@ import com.techelevator.dao.ReviewDao;
 import com.techelevator.dao.UserDao;
 import com.techelevator.model.Notification;
 import com.techelevator.model.Reviews;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,27 +29,33 @@ public class ReviewsController {
         return reviewDao.getAllReviews();
     }
 
-    @RequestMapping(value = "/doctors/{id}/reviews", method = RequestMethod.GET)
+    @RequestMapping(value = "/reviews", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reviews createReview(@RequestBody Reviews reviews) {
+        return reviewDao.createReview(new Reviews());
+    }
+
+    @RequestMapping(value = "/doctors/{doctorID}/reviews", method = RequestMethod.GET)
     public List<Reviews> getReviewsByDoctorID(@PathVariable long doctorID) {
         return reviewDao.getByDoctorID(doctorID);
     }
 
-    @RequestMapping(value = "/patients/{id}/reviews", method = RequestMethod.GET)
+    @RequestMapping(value = "/patients/{patientID}/reviews", method = RequestMethod.GET)
     public List<Reviews> getReviewsByPatientID(@PathVariable long patientID) {
         return reviewDao.getByPatientID(patientID);
     }
 
-    @RequestMapping(value = "/user/{id}/reviews/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/{patientID}/reviews/{reviewsID}", method = RequestMethod.GET)
     public List<Reviews> getReviewsByPatientIDReviewID(@PathVariable long patientID) {
         return reviewDao.getByPatientID(patientID);
     }
     //allow original reviewer to update their review, must be authenticated to access specific review/their own review
-    @RequestMapping(value = "/user/{id}/reviews/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/user/{patientID}/reviews/{reviewID}", method = RequestMethod.PUT)
     public List<Reviews> updatePatientReview(Principal principal) {
         return reviewDao.getByPatientID(Long.parseLong(principal.getName()));
     }
     //allow doctors (only) to reply to reviews
-    @RequestMapping(value = "/user/{id}/reviews/{id}", method = RequestMethod.POST)
+    @RequestMapping(value = "/user/{doctorID}/reviews/{reviewID}", method = RequestMethod.POST)
     public Reviews respondToReviews(@PathVariable long doctorID) {
         return reviewDao.getByReviewID(doctorID);
     }
