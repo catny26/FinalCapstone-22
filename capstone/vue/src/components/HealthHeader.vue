6210350eabb04e5896bfd76ecb53760545395fa5
Cathy Ngo <cngo26@gmail.com>
2022-08-12 10:20:51 -0400

    portal vue

--- capstone/vue/src/components/HealthHeader.vue
+++ capstone/vue/src/components/HealthHeader.vue
@@ -5,7 +5,7 @@
       </header>
     <div id="nav">
       <div>
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'about' }">About</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'offices' }">Offices</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'providers' }">Providers</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'patient-resources' }">Patient Resources</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'about' }">About</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'offices' }">Offices</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'providers' }">Providers</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'patient-resources' }">Patient Resources</router-link>&nbsp;|&nbsp;&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'patient-resources' }">Patient Resources</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'patient-portal' }">Patient Portal</router-link>&nbsp;|&nbsp;<router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|
       
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       </div>
