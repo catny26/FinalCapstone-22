a0ba11ba6dc4c39794f7e826f003b4b1c55de4cf
Jennifer Lee <tocontactjen@gmail.com>
2022-08-12 21:08:06 -0400

    Minor change in Agenda Controller

--- capstone/java/src/main/java/com/techelevator/controller/AgendaController.java
+++ capstone/java/src/main/java/com/techelevator/controller/AgendaController.java
@@ -18,7 +18,7 @@ public class AgendaController {
         this.agendaDao = agendaDao;
     }
 
-    @GetMapping("/agenda/{id}")
+    @GetMapping("doctors/agenda/{id}")
     public List<Agenda> getAgendaByDoctor(@PathVariable long id) {
         return agendaDao.getAgendasByDoctorId(id);
     }
