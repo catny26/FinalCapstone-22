9d289b1ecf8ac21ea0a315feb4c44b4b46f51990
Jennifer Lee <tocontactjen@gmail.com>
2022-08-14 16:35:23 -0400

    Frontend changes

--- capstone/vue/src/components/HealthHeader.vue
+++ capstone/vue/src/components/HealthHeader.vue
@@ -1,35 +1,138 @@
 <template>
   <div id="app">
-    <header>
-      <h1>TE Health</h1>
-      </header>
-    <div id="nav">
-      <div>
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'about' }">About</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'offices' }">Offices</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'providers' }">Providers</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'patient-resources' }">Patient Resources</router-link>&nbsp;|&nbsp;
-      <!-- <router-link v-bind:to="{ name: 'portal' }">Portal</router-link>&nbsp;|&nbsp; -->
-      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      </div>
+    <header class="header" id="head">
+      <img src="../assets/logote.png" alt="TE Health Logo">
+    </header>
+
+    <div class="links-container">
+      <b-nav fill class="links"
+        ><b-nav-item class="tab" style="color:white" active v-bind:to="{ name: 'home' }"
+          >Home</b-nav-item
+        >
+        <b-nav-item class="tab" active v-bind:to="{ name: 'about' }"
+          >About</b-nav-item
+        >
+        <b-nav-item class="tab" active v-bind:to="{ name: 'offices' }"
+          >Offices</b-nav-item
+        >
+        <b-nav-item class="tab" active v-bind:to="{ name: 'providers' }"
+          >Providers</b-nav-item
+        >
+        <b-nav-item class="tab" active v-bind:to="{ name: 'patient-resources' }"
+          >Patient Resources</b-nav-item
+        >
+        <b-nav-item class="tab" active v-bind:to="{ name: 'portal' }"
+          >Portal</b-nav-item
+        >
+
+        <b-nav-item
+          class="tab"
+          v-bind:to="{ name: 'login' }"
+          v-if="$store.state.token == ''"
+          active
+          >Login</b-nav-item
+        >
+        <b-nav-item
+          class="tab"
+          active
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token != ''"
+          >Logout</b-nav-item
+        >
+      </b-nav>
     </div>
     <div class="view">
-    <router-view style="margin:0 auto;"/>
-    </div> 
-    <footer style="height: 100px"/>
+      <router-view />
+    </div>
+    <footer style="height: 100px" />
   </div>
 </template>
 
 <script>
 export default {
-    name: "health-header",
-
-
-}
+  name: "health-header",
+  props: ["tabs", "fill"],
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600;800&display=swap");
+
+.header {
+  height: 120px;
+    display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  justify-items: center;  
+  background-color: #FFFFFF;
+  color: black;
+  width: 800px;
+}
+
+.header img {
+width: auto;
+height: 100%;
+
+}
+
+.header h1 {
+  width: auto;
+}
+a:hover {
+  transition: 0.7s;
+  color: white;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
+
+
+a:link {
+  color: white;
+}
+
+a:visited {
+  color: white;
+}
+
+#head h1 {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 600 !important;
+  font-size: 60px;
+  margin: 0px;
+}
+
+.links-container {
+  margin: 0px;
+  font-weight: 300;
+}
+
+.links {
+  vertical-align: top;
+  margin: 0px;
+  background-color: #2873bf;
+}
+
+.links > * {
+  margin: 0px;
+  padding-top: 10px;
+}
+
+.tab:hover {
+  background-color: #fff;
+  transition: 0.7s;
+  color: #2873bf !important;
+  text-decoration: none;
+}
+
+.tab {
+  width: auto;
+  vertical-align: top;
+  color: white !important;
+  font-weight: bold;
+  padding-bottom: 10px;
+}
 
 </style>
